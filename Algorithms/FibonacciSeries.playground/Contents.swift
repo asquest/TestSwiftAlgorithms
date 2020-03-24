efa1105e84c611fd9f01310e832d28e835930f8f
user167484 <user167484@MU045.local>
2020-03-24 20:35:39 +0530

    Fibonacci Series added to workspace

---
+++ Algorithms/FibonacciSeries.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func printFibonacciSeries(n: Int) -> Int {
+    var a = 0
+    var b = 1
+    var count = 1
+    
+    guard n > 1 else {
+        return n <= 0 ? a : b
+    }
+    while count != n {
+        b = a+b
+        a = b-a
+        count += 1
+    }
+    return b
+}
+
+printFibonacciSeries(n: 4)
