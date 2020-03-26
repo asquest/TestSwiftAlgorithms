caf426bf7aa63d13a14c4c8eb00615a849af1e64
user167484 <user167484@MU045.local>
2020-03-26 21:20:34 +0530

    Created a extension to confirm Int to a Sequence type. Need to implement Palindrome.

---
+++ Algorithms/PalindromeChecker/PalindromeChecker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PalindromeChecker
+//
+//  Created by user167484 on 3/26/20.
+//  Copyright © 2020 Allen Savio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
