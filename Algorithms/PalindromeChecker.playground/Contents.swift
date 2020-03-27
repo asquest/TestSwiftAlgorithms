1225a43ba2b522a9698b4f5c66cd2ba46e46a717
user167484 <user167484@MU045.local>
2020-03-27 07:26:53 +0530

    Did Palindrome using Stack and Sequence protocol

--- Algorithms/PalindromeChecker.playground/Contents.swift
+++ Algorithms/PalindromeChecker.playground/Contents.swift
@@ -2,31 +2,51 @@ import Foundation
 
 //var str = "Hello, playground"
 //
-//class Stack<T: Equatable> {
-//    private var arr:[T] = []
-//
-//    func pop() {
-//        guard arr.count > 0 else { return }
-//        arr.removeLast()
-//    }
-//
-//    func push(ele: T) {
-//        arr.append(ele)
-//    }
-//
-//}
-//
-//func checkPalindrome<T>(characters: T) -> Bool where T: Sequence, T.Element: Equatable {
-//    var isNotSameCount = 0
-//    for character in characters {
-//        if isNotSameCount > 1 {
-//            return false
-//        } else {
-//            if
-//        }
-//    }
-//
-//}
+class Stack<T: Equatable> {
+    private var arr:[T] = []
+    
+    var latest: T? {
+        return arr.last
+    }
+    
+    var isEmpty: Bool {
+        guard arr.count > 0 else { return true }
+        return false
+    }
+
+    func pop() {
+        guard arr.count > 0 else { return }
+        arr.removeLast()
+    }
+
+    func push(ele: T) {
+        arr.append(ele)
+    }
+    
+    
+    
+
+}
+
+func checkPalindrome<T>(characters: T) -> Bool where T: Sequence, T.Element: Equatable {
+    let stack = Stack<T.Element>()
+    let pivot: Int? = characters.count % 2 == 1 ? (characters.count + 1)/2 : nil
+    var count = 1
+    for character in characters {
+        guard count != pivot else {
+            count += 1
+            continue
+        }
+        print("In Count here \(count)")
+        if stack.latest == character {
+            stack.pop()
+        } else {
+            stack.push(ele: character)
+        }
+        count += 1
+    }
+    return stack.isEmpty
+}
 
 
 
@@ -69,9 +89,9 @@ extension Sequence {
     var count: Int { return reduce(0) { acc, row in acc + 1 } }
 }
 
-//checkPalindrome(characters: 12345)
-//checkPalindrome(characters: 12321)
-//checkPalindrome(characters: "12345")
-//checkPalindrome(characters: "12321")
-//checkPalindrome(characters: [1, 2, 3, 4, 5])
-//checkPalindrome(characters: [1, 2, 3, 2, 1])
+checkPalindrome(characters: 12345)
+checkPalindrome(characters: 12321)
+checkPalindrome(characters: "12345")
+checkPalindrome(characters: "12321")
+checkPalindrome(characters: [1, 2, 3, 4, 5])
+checkPalindrome(characters: [1, 2, 3, 2, 1])
